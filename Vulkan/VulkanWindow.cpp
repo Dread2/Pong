b0f8e9ec48cbf1c6dd7adf58e97843cb8fd294c3
Julian Tong <jueatong@gmail.com>
2024-12-30 23:28:41 -0800

    Started GLFW3 function, should draw to GPU window

---
+++ Vulkan/VulkanWindow.cpp
@@ -0,0 +1,7 @@
+#include "Vulkan/VulkanWindow.h"
+#include <iostream>
+#include <stdexcept>
+#include <GLFW/glfw3.h>
+// GLFW_INCLUDE_VULKAN must be defined for GLFW to include the Vulkan header
+// https://www.glfw.org/docs/3.3/vulkan_guide.html#vulkan_loader
+#define GLFW_INCLUDE_VULKAN
---
+++ Vulkan/VulkanWindow.h
@@ -0,0 +1,9 @@
+#ifndef VULKAN_WINDOW_H
+#define VULKAN_WINDOW_H
+
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
+void VulkanWindow(GLFWwindow* window);
+
+#endif // VULKAN_WINDOW_H
